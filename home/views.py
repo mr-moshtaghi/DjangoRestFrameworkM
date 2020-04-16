27f387321c2a120761489365e5d74264689dac07
amirbigg <amirbig44@gmail.com>
2020-04-16 22:09:03 +0430

    add ep9

--- home/views.py
+++ home/views.py
@@ -2,13 +2,14 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import PersonSerializer
 from .models import Person
+from rest_framework import status
 
 
 @api_view(['GET', 'POST'])
 def one(request):
 	if request.method == 'POST':
 		name = request.data['name']
-		return Response({'name': f'my name is {name}'})
+		return Response({'name':f'my name is {name}'})
 	else:
 		return Response({'name':'my name is amir'})
 
@@ -16,30 +17,22 @@ def one(request):
 def persons(request):
 	persons = Person.objects.all()
 	ser_data = PersonSerializer(persons, many=True)
-	return Response(ser_data.data)
+	return Response(ser_data.data, status=status.HTTP_200_OK)
 
 @api_view()
 def person(request, name):
 	try:
 		person = Person.objects.get(name=name)
 	except Person.DoesNotExist:
-		return Response({'error':'this user does not exist'})
+		return Response({'error':'this user does not exist'}, status=status.HTTP_404_NOT_FOUND)
 	ser_data = PersonSerializer(person)
-	return Response(ser_data.data)
+	return Response(ser_data.data,  status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def person_create(request):
 	info = PersonSerializer(data=request.data)
 	if info.is_valid():
-		Person(name=info.validated_data['name'], age=info.validated_data['age'], email=info.validated_data['email']).save()
-		return Response({'message':'ok'})
+		info.save()
+		return Response({'message':'ok'}, status=status.HTTP_201_CREATED)
 	else:
-		return Response(info.errors)
-
-
-
-
-
-
-
-
+		return Response(info.errors, status=status.HTTP_400_BAD_REQUEST)
