18893ee4bbfb6c4620b424c9eed45da35bdb5a68
amirbigg <amirbig44@gmail.com>
2020-04-16 14:26:30 +0430

    add ep7

--- home/serializers.py
+++ home/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
+from .models import Person
 
 
-class PersonSerializer(serializers.Serializer):
-	id = serializers.IntegerField()
-	name = serializers.CharField()
-	age = serializers.IntegerField()
-	email = serializers.EmailField()
+class PersonSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Person
+		fields = ('id', 'name', 'age', 'email')
+		extra_kwargs = {
+			'email':{'write_only':True}
+		}
