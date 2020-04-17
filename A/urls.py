05fca986577a453842faa7bdc4fc7e99eba40c80
amirbigg <amirbig44@gmail.com>
2020-04-17 13:41:21 +0430

    add ep10

--- A/urls.py
+++ A/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace='home')),
+    path('api-token-auth/', obtain_auth_token),
 ]
