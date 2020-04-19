37eae901d51e0be37246dd895f35199f879dc2dc
amirbigg <amirbig44@gmail.com>
2020-04-19 22:39:31 +0430

    add ep19

--- A/settings.py
+++ A/settings.py
@@ -142,5 +142,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework.renderers.AdminRenderer',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2
 }
